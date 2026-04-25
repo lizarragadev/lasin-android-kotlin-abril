e49bf9e5a7851665e562e8d1c2446fff84cc77da
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2026-04-24 22:46:47 -0400

    add samples

--- 03-clase02-kotlin-demo-colecciones-y-dataclass/src/main/kotlin/bo/edu/umsa/curso/clase02/colecciones/ColeccionesDemo.kt
+++ 03-clase02-kotlin-demo-colecciones-y-dataclass/src/main/kotlin/bo/edu/umsa/curso/clase02/colecciones/ColeccionesDemo.kt
@@ -19,6 +19,146 @@ fun main() {
     listasBasicasYDataClassCopy(cursos)
     filterMapGroupBy(cursos)
     sortedYDisctinct(cursos)
+    anyAllNone(cursos)
+    flatMap()
+    partitionDemo(cursos)
+    mapVsMutableMap()
+
+    ejemploClases()
+    ejemploDataClass()
+    ejemploHerencia()
+    ejemploInterfaces()
+    ejemploSealedClass()
+}
+
+class Persona(
+    val nombre: String,
+    val edad: Int
+) {
+     // no es necesario getters, setters, constructores
+
+    fun presentarse() {
+        println("Hola, soy $nombre y tengo $edad años.")
+    }
+}
+
+private fun ejemploClases() {
+    val persona = Persona("Luis", 24)
+    persona.presentarse()
+}
+
+data class Cursoo(
+    val nombre: String,
+    val creditos: Int
+)
+
+fun ejemploDataClass() {
+    val curso1 = Cursoo("Programacion II", 5)
+    val curso2 = curso1.copy(creditos = 10)
+    println(curso1)
+    println(curso2)
+}
+
+open class Animal(val nombre: String) {
+    fun respirar(){
+        println("$nombre respirando...")
+    }
+}
+
+class Perro(nombre: String): Animal(nombre) {
+    fun ladrar() {
+        println("$nombre ladrando...")
+    }
+}
+
+fun ejemploHerencia() {
+    val perro = Perro("Firulais")
+
+    perro.respirar()
+    perro.ladrar()
+}
+
+interface Volador {
+    fun volar()
+}
+
+class Pajaro: Volador {
+    override fun volar() {
+        println("El pájaro está volando")
+    }
+}
+
+class Avion: Volador {
+    override fun volar() {
+        println("El avión esta planeando")
+    }
+}
+
+fun ejemploInterfaces() {
+    val pajaro = Pajaro()
+    val avion = Avion()
+    pajaro.volar()
+    avion.volar()
+}
+
+sealed class Resultado {
+    data class Exito(val datos: String): Resultado()
+    data class Error(val mensaje: String): Resultado()
+    object Cargando: Resultado()
+}
+
+fun ejemploSealedClass() {
+    val resultado: Resultado = Resultado.Exito("Datos cargados.")
+    val resultado2: Resultado = Resultado.Error("Error en la API")
+
+    when(resultado) {
+        is Resultado.Exito -> println("Éxito: ${resultado.datos}")
+        is Resultado.Error -> println("Error: ${resultado.mensaje}")
+        is Resultado.Cargando -> println("Cargando...")
+    }
+}
+
+fun mapVsMutableMap() {
+    println("- map vs mutableMap -")
+    // mapOf, mutableMapOf
+    val ro = mapOf("a" to 1, "b" to 2)
+    val rw = mutableMapOf("x" to 10)
+
+    println("Inmutable: $ro")
+    println("Mutable: $rw")
+    rw["y"] = 20
+    rw["x"] = 5
+    println("Mutable editado: $rw")
+}
+
+fun partitionDemo(cursos: List<Curso>) {
+    println("- partition -")
+    val (inf, resto) = cursos.partition { it.carrera == "INF" }
+    println("Solo INF: $inf")
+    println("Resto: $resto")
+}
+
+fun flatMap() {
+    println("- flatMap -")
+    val porCarrera: Map<String, List<String>> = mapOf(
+        "INF" to listOf("Prog I", "BD"),
+        "MAT" to listOf("Cálculo", "Álgebra")
+    )
+    val todasLasMaterias = porCarrera.flatMap { it.value }
+    println("Materias Aplanadas: $todasLasMaterias")
+}
+
+fun anyAllNone(cursos: List<Curso>) {
+    println("- any (al menos uno cumple?), all (todos cumplen?), none(ninguno cumple?) -")
+    cursos.forEach { println(it) }
+    println("----")
+    val hayInf = cursos.any { it.carrera == "INF" }
+    val todosMasDe3 = cursos.all { it.creditos > 3 }
+    val ningunoQuim = cursos.none { it.carrera == "QUI" }
+    println("Hay INF? $hayInf")
+    println("Todos mas de 3? $todosMasDe3")
+    println("Ninguno de QUIM? $ningunoQuim")
+
 }
 
 fun sortedYDisctinct(cursos: List<Curso>) {
--- 06-clase02-android-compose-intro/app/src/main/java/bo/edu/umsa/curso/clase02/composeintro/MainActivity.kt
+++ 06-clase02-android-compose-intro/app/src/main/java/bo/edu/umsa/curso/clase02/composeintro/MainActivity.kt
@@ -1,11 +1,55 @@
 package bo.edu.umsa.curso.clase02.composeintro
 
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import bo.edu.umsa.curso.clase02.composeintro.ui.theme.Clase02ComposeIntroTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: android.os.Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Clase02ComposeIntroTheme {
+                DemoComposeIntroScreen()
+            }
+        }
+    }
+}
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun DemoComposeIntroScreen() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar(
+                title = { Text("Demo Compose Intro") },
+            )
+        },
+    ) {
+        innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(all = 16.dp)
+                .fillMaxSize()
+        ) {
+            Text("¡Hola, Compose!", style = MaterialTheme.typography.bodyLarge)
+            Text("Texto Mediano", style = MaterialTheme.typography.bodyMedium)
+            Text("Android", style = MaterialTheme.typography.bodySmall)
+        }
     }
 }
 
